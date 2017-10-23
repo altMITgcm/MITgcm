d8a9e0d8d23040423e2df4ee2c9a19d601c91319
Ed Doddridge <edoddridge@gmail.com>
2017-10-23 15:51:11 -0400

    allow for multiple subtests, expand list of tests
    The python function can now parse multiple subtests for each test, and can be passed a list of values for `threshold` (each value separated by a space from the one before it).
    The list of experiments has been expanded to include all non-adjoint tutorials.

--- verification/verification_parser.py
+++ verification/verification_parser.py
@@ -1,8 +1,19 @@
 """Parse verification outputs."""
 
 import re
+import glob
+import os
 
 def verification_parser(filename, threshold):
+
+    # function should be given a threshold value for each sub test
+    (directory, _) = os.path.split(filename)
+    # how many additional tests are run with tweaks to this configuration
+    num_exps = len(glob.glob(directory+'/input.*'))+1
+
+    assert len(threshold)==num_exps
+
+    # open the testreport output to assess pass/fail            
     with open(filename) as f:
         lines = f.readlines()
 
@@ -16,23 +27,29 @@ def verification_parser(filename, threshold):
                     # but set to false to catch next matches.
                     first_match = False
                 else:
-                    test_results = lines[i+2]
+                    # save the line number where the output is found
+                    output_line = i
+                    break
 
-        # split test_results into a list with values for each number. 
-        # this uses spaces and the < > characters to separate the numbers.
-        test_results = re.split('[ ><]',test_results)
-        # ignore the Genmake, depend, make, and run checks, as well as
-        # the "pass" or "fail" and test name at the end of the line
-        test_results = test_results[4:-3]
-        # convert to floats
-        dp_similarity = []
-        for i, x in enumerate(test_results):
-            try:
-                dp_similarity.append(float(x))
-            except ValueError:
-                pass
+        # loop through each of the subexperiments:
+        for j in xrange(len(threshold)):
+            test_results = lines[output_line+2+j]
 
-        assert all(elements > threshold for elements in dp_similarity)
+            # split test_results into a list with values for each number. 
+            # this uses spaces and the < > characters to separate the numbers.
+            test_results = re.split('[ ><]',test_results)
+            # ignore the Genmake, depend, make, and run checks, as well as
+            # the "pass" or "fail" and test name at the end of the line
+            test_results = test_results[4:-3]
+            # convert to floats
+            dp_similarity = []
+            for i, x in enumerate(test_results):
+                try:
+                    dp_similarity.append(float(x))
+                except ValueError:
+                    pass
+
+            assert all(elements > threshold[j] for elements in dp_similarity)
 
 if __name__ == '__main__':
 
@@ -43,8 +60,8 @@ if __name__ == '__main__':
     parser.add_argument('-filename', type=str, 
                         help='path to output file from the verification test')
 
-    parser.add_argument('-threshold', type=int, default=15, 
-                        help='number of decimal places of similarity required for test to pass')
+    parser.add_argument('-threshold',nargs='+', type=int, default=15, 
+                        help='number of decimal places of similarity required for test to pass. Requires a value for each sub test. Separate values with a space.')
 
     args = parser.parse_args()
 
